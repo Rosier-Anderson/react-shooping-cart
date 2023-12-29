0f5f16eb55dd8e6ff2b7c43349b15b9ed593eb2c
Anderson <97542626+rosieranderson1@users.noreply.github.com>
2023-12-29 18:45:07 +0000

    Add feching function for API request

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,44 +1,55 @@
-import React, { useState, useEffect } from "react";
+// Card.js
+import React from "react";
 import "./card.css";
+import useApiData from "./services/fechData/getApiData";
+
 export default function Card() {
-  const [productData, setProductData] = useState(null);
+  const { data, loading, error } = useApiData(
+    "https://fakestoreapi.com/products?limit=18"
+  );
 
-  useEffect(() => {
-    fetch("https://fakestoreapi.com/products?limit=18")
-      .then((res) => res.json())
-      .then((json) => setProductData(json));
-  }, []); // Empty dependency array to run the effect only once on mount
+  if (loading) {
+    return (
+      <div
+        class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+        role="status"
+      >
+        <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+          Loading...
+        </span>
+      </div>
+    );
+  }
 
-  console.log(productData);
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
 
   return (
     <>
-      {/* Assuming the  product's image, title, and price are displayed */}
-      {productData && productData.length > 0 && (
+      {data && data.length > 0 && (
         <>
-          {productData.map((item) => {
-            return (
-              <article key={item.id} className="store-product-card">
-                <img
-                  src={item.image}
-                  alt="Product Image"
-                  className="store-product-image img-style"
-                />
-                <div className="store-product-details">
-                  <p className="store-product-title">{item.title}</p>
-                  <h2 className="store-product-price">${item.price}</h2>
-                  <div className="store-product-actions">
-                    <button className="store-product-more-details">
-                      DETAILS
-                    </button>
-                    <button type="button" className="store-add-to-cart-button">
-                      ADD TO CART
-                    </button>
-                  </div>
+          {data.map((item) => (
+            <article key={item.id} className="store-product-card">
+              <img
+                src={item.image}
+                alt="Product Image"
+                className="store-product-image img-style"
+              />
+              <div className="store-product-details">
+                <p className="store-product-title">{item.title}</p>
+                <h2 className="store-product-price">${item.price}</h2>
+                <div className="store-product-actions">
+                  <button className="store-product-more-details">
+                    DETAILS
+                  </button>
+                  <button type="button" className="store-add-to-cart-button">
+                    ADD TO CART
+                  </button>
                 </div>
-              </article>
-            );
-          })}
+              </div>
+            </article>
+          ))}
         </>
       )}
     </>
