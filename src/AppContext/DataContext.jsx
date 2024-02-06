3c0106bc0308e65156867f4d0134e77241617d36
Anderson <97542626+rosieranderson1@users.noreply.github.com>
2024-02-06 02:40:51 +0000

    useCantext intergration

--- src/AppContext/DataContext.jsx
+++
@@ -1,5 +0,0 @@
-import React, { createContext } from "react";
-export const DataContext = createContext(1);
-export default function DataProvider({ children }) {
-  return <DataContext.Provider>{children}</DataContext.Provider>;
-}
